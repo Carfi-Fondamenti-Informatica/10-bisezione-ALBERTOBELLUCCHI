50d3b4cd4610b6ac46dadbc4fc4243c3cd0e2f41
Alberto_Bellucchi <115469272+ALBERTOBELLUCCHI@users.noreply.github.com>
2022-12-19 12:45:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,43 +1,43 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
-#include "cmath"
 
-float f (float y){
-    float f;
-    f = (y*y*cos(y))+1;
-    return f;
+float f(float y){
+    return (pow(y,2)*cos(y))+1;
 }
 
-bool prima(float a, float b){
-    if(f(a)*f(b)>=0){
+bool condizione(float a, float b){
+    if(f(a)*f(b)>=0)
         return true;
-    }else{
+    else
         return false;
-    }
 }
 
-
+void stampa(float x){
+    cout<<int(x*10000)/10000.0<<endl;
+}
 
 int main() {
-    float a,b,x,err;
-    do {
-        cout << "inserire estremi"<<endl;
-        cin >> a;
-        cin >> b;
-    }while(prima(a,b));
+    float a=0,b=0,x,err;
+
+    do{
+        cout<<"inserire estremi"<<endl;
+        cin>>a>>b;
+    }while(condizione(a,b));
+
     do{
         x=(a+b)/2;
-        if(f(x)==0){
-            cout<<int(x*10000)/10000.0<<endl;
-        }else {
-            if (f(a) * f(x) < 0) {
-                b = x;
-            } else {
+        if(f(x)==0)
+            break;
+        else {
+            if (condizione(a, b))
                 a = x;
-            }
+            else
+                b = x;
             err = abs((b - a) / 2);
         }
     }while(err>=1e-6);
-    cout<<int(x*10000)/10000.0<<endl;
+    stampa(x);
+
     return 0;
 }
